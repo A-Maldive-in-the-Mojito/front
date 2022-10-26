6195ffc2dc3b449468f911301f805eb3d2002c18
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-10-26 21:09:38 +0900

    10.26 React.StrictMode 제거

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ import { Provider, useSelector, useDispatch } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
+,
   document.getElementById('root')
 );
 
